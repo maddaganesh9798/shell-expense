2455645b936513c4c561c07783cb09af725ab90b
maddaganesh9798 <madda.ganesh9798@gmail.com>
2026-04-16 14:49:01 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# LOGS_FOLDER="/var/log/expense"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+# mkdir -p $LOGS_FOLDER
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -9,7 +15,8 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" 
+        # | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -24,6 +31,30 @@ CHECK_ROOT(){
 #     fi
 # }
 
-# echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" 
+# | tee -a $LOG_FILE
 
-# CHECK_ROOT
+CHECK_ROOT
+
+# dnf install mysql-server -y &>>$LOG_FILE
+# VALIDATE $? "Installing MySQL Server"
+
+# systemctl enable mysqld &>>$LOG_FILE
+# VALIDATE $? "Enabled MySQL Server"
+
+# systemctl start mysqld &>>$LOG_FILE
+# VALIDATE $? "Started MySQL server"
+
+# mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+# if [ $? -ne 0 ]
+# then
+#     echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+#     mysql_secure_installation --set-root-pass ExpenseApp@1
+#     VALIDATE $? "Setting UP root password"
+# else
+#     echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+# fi
+
+# # Assignment
+# # check MySQL Server is installed or not, enabled or not, started or not
+# # implement the above things
