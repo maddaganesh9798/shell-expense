a82d210beb18c1b5d72d4f2fdabfdb68d4c3ae64
maddaganesh9798 <madda.ganesh9798@gmail.com>
2026-04-16 14:44:55 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
-userid=(id -u)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
-if [ "$userid" -ne 0 ]; then
-    echo "Please run as root"
-    exit 1
-fi
-
-echo "I am root!"
-# userid=$(id -u)
-
-# check_root(){
-#     if [ $userid -eq 0 ]; then
-#     echo "You are running as root."
-# else
-#     echo "You are NOT running as root."
-# fi
-# }
-
-# USERID=$(id -u)
-# R="\e[31m"
-# G="\e[32m"
-# N="\e[0m"
-# Y="\e[33m"
-
-# CHECK_ROOT(){
-#     if [ $USERID -ne 0 ]
-#     then
-#         echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
-#         exit 1
-#     fi
-# }
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
 # VALIDATE(){
 #     if [ $1 -ne 0 ]
